5a617ce2e7290fd5e7cc3e5bb5e54c1b456e4d2f
moonwalker <m00n3r@protonmail.com>
2022-07-05 15:43:51 +0300

    Update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,6 +6,7 @@ import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
+import "hardhat-deploy";
 
 dotenv.config();
 
@@ -19,16 +20,70 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
+const accounts = {
+  mnemonic: process.env.MNEMONIC || "",
+};
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
   networks: {
+    mainnet: {
+      url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts,
+      chainId: 1,
+      tags: ["production"],
+    },
+    localhost: {
+      accounts,
+      live: false,
+      tags: ["local"],
+    },
+    hardhat: {
+      accounts,
+      forking: {
+        enabled: process.env.HARDHAT_NETWORK_FORKING === "true",
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+        blockNumber: Number(process.env.HARDHAT_NETWORK_BLOCK),
+      },
+      live: false,
+      tags: ["local"],
+      chainId: 1337,
+      // mining: {
+      //   auto: false,
+      //   interval: [1000, 3000],
+      // },
+    },
     ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts,
+      chainId: 3,
+      tags: ["staging"],
+    },
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts,
+      chainId: 4,
+      tags: ["staging"],
+    },
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts,
+      chainId: 5,
+      tags: ["staging"],
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      // 1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    },
+    team: {
+      default: 1, // here this will by default take the second account as team (so in the test this will be a different account than the deployer)
+      // 1: "", // on the mainnet the team could be a multi sig
+      // 4: "", // on rinkeby
     },
   },
   gasReporter: {
